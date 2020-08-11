d5e71e9037d8b0855d2d640d74ffc6058c80f9af
Joanna Moura <joannasmoura@hotmail.com>
2020-08-11 01:29:47 -0300

    Criação das urls de logine refresh-token

--- backend/intmed/intmed/settings.py
+++ backend/intmed/intmed/settings.py
@@ -113,14 +113,16 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ],
 }
 
--- backend/intmed/intmed/urls.py
+++ backend/intmed/intmed/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import include, path
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_jwt.views import obtain_jwt_token,refresh_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('clinica.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-    # path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^login/', obtain_jwt_token),
+    url(r'^refresh-token/', refresh_jwt_token),
 ]
